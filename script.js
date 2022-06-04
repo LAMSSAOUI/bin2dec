7e0618ccecc7a2039db95e45ec30ea06356bea9a
LAMSSAOUI <lam998216@gmail.com>
2022-06-04 04:24:50 -0700

    some modifications

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const number_taken = document.getElementById("binary") ; 
-const decimal_number = document.getElementById("") ; 
-let puiss = number_taken.length-1 ; 
-var arr = [] ; 
-var arr2 = [] ; 
 
 
-() => {
-  for (i=0; i<number_taken.length; i++){
+
+function fetching() {
+  var mod ; 
+  var quo ; 
+  for (let i=0; i<number_taken.length; i++){
     mod  = number_taken % 10 ; 
   // modulo push it in an Array 
       arr.push(mod) ; 
@@ -18,8 +16,10 @@ var arr2 = [] ;
 
 // start sorting elements with multiply every element with pow(2,puiss) with addition 
     // and puiss = puiss -1  ; 
-(arr) => { 
-  for(i=0;i<arr.length;i++){
+function mul(arr) { 
+  var mul ; 
+  var produit ; 
+  for(let i=0;i<arr.length;i++){
     mul = Math.pow(2,puiss) ; 
     produit = arr[i]*mul ;
     arr2.push(produit) ;
@@ -29,9 +29,21 @@ return arr2 ;
 }
 
 
- (arr2) => { 
-  for(i=0;i<arr2.length;i++){
+ function somme(arr2)  { 
+  for(let i=0;i<arr2.length;i++){
     return arr2[i]+arr2[i+1] ; 
 }
 }
+function convert_to_decimal(){
+  const number_taken = document.getElementById("binary") ; 
+  let puiss = number_taken.length-1 ; 
+  var arr = [] ; 
+  var arr2 = [] ; 
 
+  arr = fetching() ; 
+  arr2 = mul(arr) ; 
+  decimal_num = somme(arr2); 
+  
+    return decimal_num ; 
+
+}
