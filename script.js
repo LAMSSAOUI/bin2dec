4a3891c07ca6174e22aa385ba148b1a83931295b
LAMSSAOUI <lam998216@gmail.com>
2022-06-04 03:28:37 -0700

    i modify  my fn bin2dec

---
+++ script.js
@@ -0,0 +1,17 @@
+const number_taken = document.getElementById("binary") ; 
+const decimal_number = document.getElementById("") ; 
+let puiss = number_taken.length-1 ; 
+
+for (i=0; i<number_taken.length; i++){
+    mod  = number_taken % 10 ; 
+  // modulo push it in an Array 
+    quo = number_taken/10 ; 
+    number_taken = quo ; 
+
+    // start sorting elements with multiply every element with pow(2,puiss) with addition 
+    // and puiss = puiss -1  ; 
+
+
+}
+
+
